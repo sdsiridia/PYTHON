00d692080f1bc6b4c210e003063775d517c1942b
Pablo Rossa <pablorossa@gmail.com>
2024-09-12 14:33:28 +0200

    2 versiones

--- leer_correo_de_gmail.py
+++ leer_correo_de_gmail.py
@@ -2,7 +2,7 @@
 
 import imaplib
 import email
-from email.header import decode_header
+from email.header import decode_header  # metodo para leer el encabezado
 import webbrowser
 import os
 # import getpass -> PARA USAR CUANDO QUEREMOS ESCRIBIR LA CONTRASEÑA
@@ -16,25 +16,27 @@ PASSWORD = 'ljtckojprksyeiyz'  # getpass("PASSWORD: ")
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 # iniciar sesión
 imap.login(USERNAME, PASSWORD)
-# por algun  otivo lee siempre el mismo email con la opcion ("INBOX",readonly=False) deberia dejar pero no
+#  lee siempre los email por fecha con la opcion
+# ("INBOX",readonly=False) deberia saltearlos pero no lo hace
 status, mensajes = imap.select("INBOX")
-# print(mensajes) # -> podemos saber cuantos correos tenemmos
+print(mensajes)  # -> podemos saber cuantos correos tenemmos
+print('\n=============\n')
 # mensajes a recibir
-N = 1
+N = 3
 # cantidad total de correos
-mensajes = int(mensajes[0])
+can_mensajes = int(mensajes[0])
 
-for i in range(mensajes, mensajes - N, -1):
+for i in range(can_mensajes, can_mensajes - N, -1):
     #    print(f"vamos por el mensaje: {i}")
     #     # Obtener el mensaje
-    try:  # -> por si la camntidad de mensajes que quiero leer es superior a la que existen
-        # fetch -> obtener el mensaje i RFC822 -> que voy as pedir todas las partes del mensaje
+    try:  # -> por si la cantidad de mensajes que quiero leer es superior a la que existen
+        # fetch -> obtener el mensaje i RFC822 -> es un standar dice que voy as pedir todas las partes del mensaje
         # las variables res y mensaje es por que 'imap.fetch(str(i), "(RFC822)")'
         # nos devuelve la respuesta de la solicitud y el mensaje
-        # srt(i) -> i es el numero de mensaje
+        # srt(i) -> i es el numero de mensaje | metodo fetch pide un string
         res, mensaje = imap.fetch(str(i), "(RFC822)")
         # print(mensaje) -> para mostrar por consola los mensajes
-        # está de una manera un poco ilejible
+        # está de una manera poco lejible
     # cuando nos referimos a un error especifco colocamos ImportError
     except ImportError:
         break
@@ -53,7 +55,8 @@ for i in range(mensajes, mensajes - N, -1):
             from_ = mensaje.get("From")
             print("Asunto:", subject)
             print("From:", from_)
-            print("Mensaje obtenido con exito")
+            print("******Mensaje obtenido con exito******")
+            print("\n\n=====================\n\n")
 #             # si el correo es html
 # ------------------------------------------------------------------------
             if mensaje.is_multipart():
@@ -87,59 +90,59 @@ for i in range(mensajes, mensajes - N, -1):
 
 #                         print(str(body))
 # leo el adjunto y lo guardo en una carpeta
-                    elif "attachment" in CONTENT_DISPOSITION:
-                        # download attachment y obtenemos el nombre
-                        nombre_archivo = part.get_filename()
-                        if nombre_archivo:
-                            # corroboramos si existe la carpeta
-                            if not os.path.isdir(subject):
-                                # crear una carpeta para el mensaje
-                                # con el nombre del asunto
-                                os.mkdir(subject)
-                                # la ruta del archivo que voy a crear
-                                # lo hacemos asi para que sea compatible con multiples sistemas
-                                # ya se linux windows etc
-                                # os.path.join -> identifica el sistema operativo
-                            ruta_archivo = os.path.join(
-                                subject, nombre_archivo)
-                            # descargamos el adjunto y lo guardamos
-                            # se hace con 'wb' -> para que python lo escriba como bytes
-                            with open(ruta_archivo, "wb") as file:
-                                file.write(part.get_payload(decode=True))
-# ---------------------------------------------------------------------------
-# ahora agregamos una parte para reconocer el contenido html del mensaje como es debido
-# y no que lo pase como texto plano en la consola
-# si no tiene adjunto
-            else:
-                # contenido del mensaje
-                content_type = mensaje.get_content_type()
-                # cuerpo del mensaje decode=True decodifica el contenido
-                body = mensaje.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-                    # intento que guarde el cuerpo en un archivo
-                    try:
-                        with open(ruta_archivo, 'w', encoding='utf-8') as file:
-                            file.write(body)
-                    except ImportError:
-                        pass
-#                     # mostrar solo el texto
-                    print(body)
-            if content_type == "text/html":
-                #                 # Abrir el html en el navegador
-                if not os.path.isdir(subject):
-                    os.mkdir(subject)
-                nombre_archivo = f"{subject}.html"
-                ruta_archivo = os.path.join(subject, nombre_archivo)
-                with open(ruta_archivo, "w", encoding='utf-8') as file:
-                    # decoded_string = body.decode('utf-8')
-                    # por algun motivi da un error aunque funciona con la codificación
-                    # 'utf-8' puedo cambiar la por 'latin-1' pero pierdo todo
-                    # lo que embellece html y se queda como texto plano con caracteres extraños
-                    decoded_string = body.decode('utf-8')
-                    # decoded_string = str(body,'latin-1')
-                    file.write(decoded_string)
-#                 # abrir el navegador para mostrar el archivo
-                webbrowser.open(ruta_archivo)
+#                     elif "attachment" in CONTENT_DISPOSITION:
+#                         # download attachment y obtenemos el nombre
+#                         nombre_archivo = part.get_filename()
+#                         if nombre_archivo:
+#                             # corroboramos si existe la carpeta
+#                             if not os.path.isdir(subject):
+#                                 # crear una carpeta para el mensaje
+#                                 # con el nombre del asunto
+#                                 os.mkdir(subject)
+#                                 # la ruta del archivo que voy a crear
+#                                 # lo hacemos asi para que sea compatible con multiples sistemas
+#                                 # ya se linux windows etc
+#                                 # os.path.join -> identifica el sistema operativo
+#                             ruta_archivo = os.path.join(
+#                                 subject, nombre_archivo)
+#                             # descargamos el adjunto y lo guardamos
+#                             # se hace con 'wb' -> para que python lo escriba como bytes
+#                             with open(ruta_archivo, "wb") as file:
+#                                 file.write(part.get_payload(decode=True))
+# # ---------------------------------------------------------------------------
+# # ahora agregamos una parte para reconocer el contenido html del mensaje como es debido
+# # y no que lo pase como texto plano en la consola
+# # si no tiene adjunto
+#             else:
+#                 # contenido del mensaje
+#                 content_type = mensaje.get_content_type()
+#                 # cuerpo del mensaje decode=True decodifica el contenido
+#                 body = mensaje.get_payload(decode=True).decode()
+#                 if content_type == "text/plain":
+#                     # intento que guarde el cuerpo en un archivo
+#                     try:
+#                         with open(ruta_archivo, 'w', encoding='utf-8') as file:
+#                             file.write(body)
+#                     except ImportError:
+#                         pass
+# #                     # mostrar solo el texto
+#                     print(body)
+#             if content_type == "text/html":
+#                 #                 # Abrir el html en el navegador
+#                 if not os.path.isdir(subject):
+#                     os.mkdir(subject)
+#                 nombre_archivo = f"{subject}.html"
+#                 ruta_archivo = os.path.join(subject, nombre_archivo)
+#                 with open(ruta_archivo, "w", encoding='utf-8') as file:
+#                     # decoded_string = body.decode('utf-8')
+#                     # por algun motivi da un error aunque funciona con la codificación
+#                     # 'utf-8' puedo cambiar la por 'latin-1' pero pierdo todo
+#                     # lo que embellece html y se queda como texto plano con caracteres extraños
+#                     decoded_string = body.decode('utf-8')
+#                     # decoded_string = str(body,'latin-1')
+#                     file.write(decoded_string)
+# #                 # abrir el navegador para mostrar el archivo
+#                 webbrowser.open(ruta_archivo)
             print("********************************")
 imap.close()
 imap.logout()
---
+++ leer_correo_no_leido.py
@@ -0,0 +1,41 @@
+'''Leer correos no leidos y sacar su asunto, orign y cuerpo'''
+import imaplib
+import email
+
+# Conectar a la cuenta de Gmail
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login('sdsiridia@gmail.com', 'ljtckojprksyeiyz')
+
+# Seleccionar la bandeja de entrada
+mail.select('inbox')
+
+# Buscar correos no leídos
+status, messages = mail.search(None, 'UNSEEN')
+
+# Obtener la lista de IDs de correos no leídos
+mail_ids = messages[0].split()
+# print(mail_ids)
+for i, mail_id in enumerate(mail_ids):
+    status, msg_data = mail.fetch(mail_id, '(RFC822)')
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            msg = email.message_from_bytes(response_part[1])
+            subject = msg['subject']
+            from_ = msg['from']
+            print(f'From: {from_}\nSubject: {subject}\n')
+
+            # Obtener el cuerpo del mensaje
+            if msg.is_multipart():
+                for part in msg.walk():
+                    if part.get_content_type() == 'text/plain':
+                        body = part.get_payload(decode=True).decode()
+                        print('\n=================\nMultipart\n=================\n')
+                        print(f'Body: {body}\n')
+                        print('\n=================\nMultipart\n=================\n')
+            else:
+                body = msg.get_payload(decode=True).decode()
+                print('\n=================\nnormal\n=================\n')
+                print(f'Body: {body}\n')
+                print('\n=================\nnormal\n=================\n')
+    if i > 3:
+        break

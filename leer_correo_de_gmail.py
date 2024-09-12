245323126fd08b9f6431a01b28915bad473d2500
Pablo Rossa <pablorossa@gmail.com>
2024-09-12 14:56:43 +0200

    Archivo comentado

--- leer_correo_de_gmail.py
+++ leer_correo_de_gmail.py
@@ -41,12 +41,13 @@ for i in range(can_mensajes, can_mensajes - N, -1):
     except ImportError:
         break
     for respuesta in mensaje:  # mensaje es una lista de todas las partes del correo
+        # esto es una validación, para saber si respuesta es una tupla
         if isinstance(respuesta, tuple):
             # Obtener el contenido
             # parte del contenido son tuplas por ejemplo: subject : para maría
             # tupla ->(subject:'para maria')
             mensaje = email.message_from_bytes(respuesta[1])
-            # decodificar el contenido
+            # decodificar el contenido | [0][0] -> para leer solo lo priemro
             subject = decode_header(mensaje["Subject"])[0][0]
             if isinstance(subject, bytes):
                 # convertir a string usa la misma variable pero no es necesario
@@ -80,69 +81,69 @@ for i in range(can_mensajes, can_mensajes - N, -1):
                         body = body.decode('utf-8')
                         print(body)
 #                         # Mostrar el cuerpo del correo
-#                         nombre_archivo = part.get_filename()
-#                         if nombre_archivo:
-#                             if not os.path.isdir(subject):
-#                                 # crear una carpeta para el mensaje
-#                                 os.mkdir(subject)
-#                             ruta_archivo = os.path.join(
-#                                 subject, nombre_archivo)
+                        nombre_archivo = part.get_filename()
+                        if nombre_archivo:
+                            if not os.path.isdir(subject):
+                                # crear una carpeta para el mensaje
+                                os.mkdir(subject)
+                            ruta_archivo = os.path.join(
+                                subject, nombre_archivo)
 
 #                         print(str(body))
 # leo el adjunto y lo guardo en una carpeta
-#                     elif "attachment" in CONTENT_DISPOSITION:
-#                         # download attachment y obtenemos el nombre
-#                         nombre_archivo = part.get_filename()
-#                         if nombre_archivo:
-#                             # corroboramos si existe la carpeta
-#                             if not os.path.isdir(subject):
-#                                 # crear una carpeta para el mensaje
-#                                 # con el nombre del asunto
-#                                 os.mkdir(subject)
-#                                 # la ruta del archivo que voy a crear
-#                                 # lo hacemos asi para que sea compatible con multiples sistemas
-#                                 # ya se linux windows etc
-#                                 # os.path.join -> identifica el sistema operativo
-#                             ruta_archivo = os.path.join(
-#                                 subject, nombre_archivo)
-#                             # descargamos el adjunto y lo guardamos
-#                             # se hace con 'wb' -> para que python lo escriba como bytes
-#                             with open(ruta_archivo, "wb") as file:
-#                                 file.write(part.get_payload(decode=True))
+                    elif "attachment" in CONTENT_DISPOSITION:
+                        # download attachment y obtenemos el nombre
+                        nombre_archivo = part.get_filename()
+                        if nombre_archivo:
+                            # corroboramos si existe la carpeta
+                            if not os.path.isdir(subject):
+                                # crear una carpeta para el mensaje
+                                # con el nombre del asunto
+                                os.mkdir(subject)
+                                # la ruta del archivo que voy a crear
+                                # lo hacemos asi para que sea compatible con multiples sistemas
+                                # ya se linux windows etc
+                                # os.path.join -> identifica el sistema operativo
+                            ruta_archivo = os.path.join(
+                                subject, nombre_archivo)
+                            # descargamos el adjunto y lo guardamos
+                            # se hace con 'wb' -> para que python lo escriba como bytes
+                            with open(ruta_archivo, "wb") as file:
+                                file.write(part.get_payload(decode=True))
 # # ---------------------------------------------------------------------------
 # # ahora agregamos una parte para reconocer el contenido html del mensaje como es debido
 # # y no que lo pase como texto plano en la consola
 # # si no tiene adjunto
-#             else:
-#                 # contenido del mensaje
-#                 content_type = mensaje.get_content_type()
-#                 # cuerpo del mensaje decode=True decodifica el contenido
-#                 body = mensaje.get_payload(decode=True).decode()
-#                 if content_type == "text/plain":
-#                     # intento que guarde el cuerpo en un archivo
-#                     try:
-#                         with open(ruta_archivo, 'w', encoding='utf-8') as file:
-#                             file.write(body)
-#                     except ImportError:
-#                         pass
-# #                     # mostrar solo el texto
-#                     print(body)
-#             if content_type == "text/html":
-#                 #                 # Abrir el html en el navegador
-#                 if not os.path.isdir(subject):
-#                     os.mkdir(subject)
-#                 nombre_archivo = f"{subject}.html"
-#                 ruta_archivo = os.path.join(subject, nombre_archivo)
-#                 with open(ruta_archivo, "w", encoding='utf-8') as file:
-#                     # decoded_string = body.decode('utf-8')
-#                     # por algun motivi da un error aunque funciona con la codificación
-#                     # 'utf-8' puedo cambiar la por 'latin-1' pero pierdo todo
-#                     # lo que embellece html y se queda como texto plano con caracteres extraños
-#                     decoded_string = body.decode('utf-8')
-#                     # decoded_string = str(body,'latin-1')
-#                     file.write(decoded_string)
-# #                 # abrir el navegador para mostrar el archivo
-#                 webbrowser.open(ruta_archivo)
+            else:
+                # contenido del mensaje
+                content_type = mensaje.get_content_type()
+                # cuerpo del mensaje decode=True decodifica el contenido
+                body = mensaje.get_payload(decode=True).decode()
+                if content_type == "text/plain":
+                    # intento que guarde el cuerpo en un archivo
+                    try:
+                        with open(ruta_archivo, 'w', encoding='utf-8') as file:
+                            file.write(body)
+                    except ImportError:
+                        pass
+#                     # mostrar solo el texto
+                    print(body)
+            if content_type == "text/html":
+                #                 # Abrir el html en el navegador
+                if not os.path.isdir(subject):
+                    os.mkdir(subject)
+                    nombre_archivo = f"{subject}.html"
+                    ruta_archivo = os.path.join(subject, nombre_archivo)
+                with open(ruta_archivo, "w", encoding='utf-8') as file:
+                    # decoded_string = body.decode('utf-8')
+                    # por algun motivi da un error aunque funciona con la codificación
+                    # 'utf-8' puedo cambiar la por 'latin-1' pero pierdo todo
+                    # lo que embellece html y se queda como texto plano con caracteres extraños
+                    decoded_string = body.decode('utf-8')
+                    # decoded_string = str(body,'latin-1')
+                    file.write(decoded_string)
+#                 # abrir el navegador para mostrar el archivo
+                webbrowser.open(ruta_archivo)
             print("********************************")
 imap.close()
 imap.logout()

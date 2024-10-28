d70a0d5f7a74656552fb5a65c0d139fb2f1f00a1
Pablo Rossa <pablorossa@gmail.com>
2024-10-28 17:10:35 +0100

    streamlit 2

--- streamlit_first_app.py
+++ streamlit_first_app.py
@@ -6,6 +6,7 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 import numpy as np
+import time
 
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
@@ -61,3 +62,59 @@ st.text_input("Ingresa tu nombre", key="name")
 
 # llama a la variable
 st.session_state.name
+
+st.write("Caja de texto:")
+# si no hago click no me muestra los datos
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+
+    chart_data
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+option = st.selectbox(
+    'Which number do you like best?',
+    df['first column'])
+
+'You selected: ', option
+
+
+# # Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar_01 = st.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar_01.progress(i + 1)
+    time.sleep(0.1)

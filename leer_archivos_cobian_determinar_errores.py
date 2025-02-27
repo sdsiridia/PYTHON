d821d97477f9f38002788d328922e1e17f398f6b
Pablo Rossa <pablorossa@gmail.com>
2025-02-27 13:47:47 +0100

    corrigiendo el principal por que gemini marca todos los email leidos

--- leer_archivos_cobian_determinar_errores.py
+++ leer_archivos_cobian_determinar_errores.py
@@ -1,4 +1,5 @@
 '''Leer correos no leidos y sacar su asunto, origen y cuerpo'''
+from colorama import Fore, Style, init, Back
 import imaplib
 import email
 import time
@@ -98,11 +99,13 @@ for i, mail_id in enumerate(mail_ids):
 mail.close()
 mail.logout()
 # creacion del titulo
-TEXTO = pyfiglet.figlet_format("Errores")
-print(TEXTO)
+TEXTO = pyfiglet.figlet_format("Backup", font="slant")
+# print(TEXTO)
 # armamos la tabla para presentar los datos
 # print(dic_errores)
 if len(dic_errores) > 0:
+    init()
+    print(Fore.RED + TEXTO)
     table = Table()
     table.add_column("Origen", justify="center")
     table.add_column("ERRORES", justify="center")
@@ -118,6 +121,8 @@ if len(dic_errores) > 0:
                 table.add_row(f"[bold red]{i}.- {clave}[/bold red]", f"[bold red] {
                     valor}[/bold red] :pile_of_poo:")
 else:
+    init()
+    print(Fore.GREEN + TEXTO)
     table1 = Table()
     table1.add_column("[bold green]MENSAJES[/bold green]", justify="center")
     with Live(table1, refresh_per_second=4):
